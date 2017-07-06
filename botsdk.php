f9988208d0435a92958967a14e61a55976cc7540
Panachai <panachai.ny@gmail.com>
2017-07-06 17:35:21 +0700

    last test SDK 8

--- botsdk.php
+++ botsdk.php
@@ -1,6 +1,6 @@
 <?php
 echo "before ok";
-$ composer require linecorp/line-bot-sdk
+// $ composer require linecorp/line-bot-sdk
 
 // $access_token = 'REu678+TOo+xbHA8QWYQCbQmPt5Sj4w1HKeC3wLvdzDlgKNM6RikwsMB79nMz6AlXQo8ozkIIUPbDbrnNH9OBuqJhXVATMopsukLWQV+FvmXsCpd1rubnXKLz/2ySBTZRttlRKNqAGEP1ceiMYajlwdB04t89/1O/w1cDnyilFU=';
 // $channelSecret = '0d063d59744bc653dde526c51ad431db';
