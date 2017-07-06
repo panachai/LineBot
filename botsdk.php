28d3f9e77c4531fdc4196ac0feb9eb8ba9b5a0c7
GOLF_PANACHAI_NIYOMKODCHAKORN <panachai.ny@gmail.com>
2017-07-06 22:02:43 +0700

    update line api

--- botsdk.php
+++ botsdk.php
@@ -1,13 +1,37 @@
 <?php
 echo "before ok";
+//
+// $access_token = 'REu678+TOo+xbHA8QWYQCbQmPt5Sj4w1HKeC3wLvdzDlgKNM6RikwsMB79nMz6AlXQo8ozkIIUPbDbrnNH9OBuqJhXVATMopsukLWQV+FvmXsCpd1rubnXKLz/2ySBTZRttlRKNqAGEP1ceiMYajlwdB04t89/1O/w1cDnyilFU=';
+// $channelSecret = '0d063d59744bc653dde526c51ad431db';
+//
+// $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+// $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+//
+// $response = $bot->replyText('<reply token>', 'hello!');
 
-$access_token = 'REu678+TOo+xbHA8QWYQCbQmPt5Sj4w1HKeC3wLvdzDlgKNM6RikwsMB79nMz6AlXQo8ozkIIUPbDbrnNH9OBuqJhXVATMopsukLWQV+FvmXsCpd1rubnXKLz/2ySBTZRttlRKNqAGEP1ceiMYajlwdB04t89/1O/w1cDnyilFU=';
+// example: https://github.com/onlinetuts/line-bot-api/blob/master/php/example/chapter-01.php
+
+include ('line-bot-api/php/line-bot.php');
+
 $channelSecret = '0d063d59744bc653dde526c51ad431db';
+$access_token  = 'REu678+TOo+xbHA8QWYQCbQmPt5Sj4w1HKeC3wLvdzDlgKNM6RikwsMB79nMz6AlXQo8ozkIIUPbDbrnNH9OBuqJhXVATMopsukLWQV+FvmXsCpd1rubnXKLz/2ySBTZRttlRKNqAGEP1ceiMYajlwdB04t89/1O/w1cDnyilFU=';
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+$bot = new BOT_API($channelSecret, $access_token);
 
-$response = $bot->replyText('<reply token>', 'hello!');
+if (!empty($bot->isEvents)) {
+
+	$bot->replyMessageNew($bot->replyToken, json_encode($bot->message));
+
+	if ($bot->isSuccess()) {
+		echo 'Succeeded!';
+		exit();
+	}
+
+	// Failed
+	echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody();
+	exit();
+
+}
 
 echo "ok";
 ?>
