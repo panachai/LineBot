de8b19360a28e13a2e30935f7045ac906046c1dc
GOLF_PANACHAI_NIYOMKODCHAKORN <panachai.ny@gmail.com>
2017-07-08 01:30:32 +0700

    $arrayETH completed

--- BxApiETH.php
+++ BxApiETH.php
@@ -9,23 +9,24 @@ $json1 = file_get_contents($urlapibx);
 $jsonIterator = new RecursiveIteratorIterator(
     new RecursiveArrayIterator(json_decode($json1, TRUE)),
     RecursiveIteratorIterator::SELF_FIRST);
+$arrayETH = array();
 
 foreach ($jsonIterator as $key => $val) {
 			if(is_array($val)) { //for ETH
         if($key == '21'){
-          echo "$key:<br>";
-          $val['pairing_id'];
-          $val['primary_currency'];
-          $val['secondary_currency'];
-          $val['change'];
-          $val['last_price'];
-          $val['volume_24hours'];
+          //echo "$key:<br>";
+          // $val['pairing_id'];
+          // $val['primary_currency'];
+          // $val['secondary_currency'];
+          // $val['change'];
+          // $val['last_price'];
+          // $val['volume_24hours'];
+          $arrayETH = $val;
           //echo $val['orderbook']; //รอทำต่อ
         }
 		} else {
 			//echo "$key => $val<br>";
 		}
 }
-
 
  ?>
