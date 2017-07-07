74b2942290299c3d9e3c067f4992c0e278070e3b
GOLF_PANACHAI_NIYOMKODCHAKORN <panachai.ny@gmail.com>
2017-07-08 01:31:12 +0700

    set time to 23.00 - 6.00 no alert

--- botsend.php
+++ botsend.php
@@ -3,24 +3,42 @@
 <?php
 
 // example: https://github.com/onlinetuts/line-bot-api/blob/master/php/example/chapter-02.php
+date_default_timezone_set('Asia/Bangkok');
+if(!(date("H")<6 || date("H")>23)){ //control date
+	include ('line-bot.php');
+	include ('BxApiETH.php');
 
-include ('line-bot.php');
+	$channelSecret = '0d063d59744bc653dde526c51ad431db';
+	$access_token  = 'REu678+TOo+xbHA8QWYQCbQmPt5Sj4w1HKeC3wLvdzDlgKNM6RikwsMB79nMz6AlXQo8ozkIIUPbDbrnNH9OBuqJhXVATMopsukLWQV+FvmXsCpd1rubnXKLz/2ySBTZRttlRKNqAGEP1ceiMYajlwdB04t89/1O/w1cDnyilFU=';
 
-$channelSecret = '0d063d59744bc653dde526c51ad431db';
-$access_token  = 'REu678+TOo+xbHA8QWYQCbQmPt5Sj4w1HKeC3wLvdzDlgKNM6RikwsMB79nMz6AlXQo8ozkIIUPbDbrnNH9OBuqJhXVATMopsukLWQV+FvmXsCpd1rubnXKLz/2ySBTZRttlRKNqAGEP1ceiMYajlwdB04t89/1O/w1cDnyilFU=';
+	$bot = new BOT_API($channelSecret, $access_token);
+	//$arrayETH
+	// $val['pairing_id'];
+	// $val['primary_currency'];
+	// $val['secondary_currency'];
+	// $val['change'];
+	// $val['last_price'];
+	// $val['volume_24hours'];
 
-$bot = new BOT_API($channelSecret, $access_token);
+	$bot->sendMessageNew('U291587c13144989597448437db2b8555', $arrayETH['primary_currency'].' : '.$arrayETH['secondary_currency'].'
+	change : '.$arrayETH['change'].'
+	LastPrice : '.$arrayETH['last_price'].'
+	volume_24hours : '.$arrayETH['volume_24hours']);
+	// $bot->sendMessageNew('U291587c13144989597448437db2b8555', 'Hello World !!');
+	// $bot->sendMessageNew('U291587c13144989597448437db2b8555', 'Hello World !!');
+	// $bot->sendMessageNew('U291587c13144989597448437db2b8555', 'Hello World !!');
 
-$bot->sendMessageNew('U291587c13144989597448437db2b8555', 'Hello World !!');
+	if ($bot->isSuccess()) {
+		echo 'Succeeded!';
+		exit();
+	}
 
-if ($bot->isSuccess()) {
-	echo 'Succeeded!';
+	// Failed
+	echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody();
 	exit();
+
 }
 
-// Failed
-echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody();
-exit();
 
 //linux code : curl -X POST -H 'Content-Type: application/json; charser=UTF-8' -H 'Authorization: Bearer [Channel Access Token]' -d '{ "to" : "[Your userId / User Id]", "messages" : [{ "type": "text", "text": "Server 127.0.0.1 have SSH login" }]}' https://api.line.me/v2/bot/message/push
  ?>
