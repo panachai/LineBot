a93696b39ed1eb7941a7e07ba9bf948832c67ba8
GOLF_PANACHAI_NIYOMKODCHAKORN <panachai.ny@gmail.com>
2017-07-08 01:07:15 +0700

    api complete

--- BxApiETH.php
+++ BxApiETH.php
@@ -1,12 +1,31 @@
 <?php
 
-//https://bx.in.th/api/
+echo $urlapibx = 'https://bx.in.th/api/';
+echo '<br>';
+//$urlapibx = 'https://bx.in.th/api/balance/';
 
+$json1 = file_get_contents($urlapibx);
 
-$json = '{"a":1,"b":2,"c":3,"d":4,"e":5}';
+$jsonIterator = new RecursiveIteratorIterator(
+    new RecursiveArrayIterator(json_decode($json1, TRUE)),
+    RecursiveIteratorIterator::SELF_FIRST);
 
-$obj = json_decode($json);
-print $obj->{'a'}; // 12345
-print $obj->{'b'}; // 12345
+foreach ($jsonIterator as $key => $val) {
+			if(is_array($val)) { //for ETH
+        if($key == '21'){
+          echo "$key:<br>";
+          $val['pairing_id'];
+          $val['primary_currency'];
+          $val['secondary_currency'];
+          $val['change'];
+          $val['last_price'];
+          $val['volume_24hours'];
+          //echo $val['orderbook']; //รอทำต่อ
+        }
+		} else {
+			//echo "$key => $val<br>";
+		}
+}
+
 
  ?>
