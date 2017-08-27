59ff6475839ac9aa7bd5aee1bc1487102954a1c9
GOLF_PANACHAI_NIYOMKODCHAKORN <panachai.ny@gmail.com>
2017-08-27 19:05:02 +0700

    send omg

--- botsend.php
+++ botsend.php
@@ -15,10 +15,14 @@
 	// $val['last_price'];
 	// $val['volume_24hours'];
 
-	$bot->sendMessageNew('U291587c13144989597448437db2b8555', $arrayETH['primary_currency'].' : '.$arrayETH['secondary_currency'].'
+	$bot->sendMessageNew('U291587c13144989597448437db2b8555',
+	$arrayETH['primary_currency'].' : '.$arrayETH['secondary_currency'].'
 	change : '.$arrayETH['change'].'
 	LastPrice : '.$arrayETH['last_price'].'
-	volume_24hours : '.$arrayETH['volume_24hours']);
+	volume_24hours : '.$arrayETH['volume_24hours'].
+	$arrayOMG['primary_currency'].' : '.$arrayOMG['secondary_currency'].'
+	LastPrice : '.$arrayOMG['last_price'].'
+	volume_24hours : '.$arrayOMG['volume_24hours']);
 	// $bot->sendMessageNew('U291587c13144989597448437db2b8555', 'Hello World !!');
 	// $bot->sendMessageNew('U291587c13144989597448437db2b8555', 'Hello World !!');
 	// $bot->sendMessageNew('U291587c13144989597448437db2b8555', 'Hello World !!');
