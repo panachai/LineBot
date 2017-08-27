a48f0aa0f3cba5ccccb42ca6b5e7706da730da67
GOLF_PANACHAI_NIYOMKODCHAKORN <panachai.ny@gmail.com>
2017-08-27 19:07:16 +0700

    format data

--- botsend.php
+++ botsend.php
@@ -17,12 +17,13 @@
 
 	$bot->sendMessageNew('U291587c13144989597448437db2b8555',
 	$arrayETH['primary_currency'].' : '.$arrayETH['secondary_currency'].'
-	change : '.$arrayETH['change'].'
 	LastPrice : '.$arrayETH['last_price'].'
-	volume_24hours : '.$arrayETH['volume_24hours'].
+	volume_24hours : '.$arrayETH['volume_24hours'].''.
 	$arrayOMG['primary_currency'].' : '.$arrayOMG['secondary_currency'].'
 	LastPrice : '.$arrayOMG['last_price'].'
 	volume_24hours : '.$arrayOMG['volume_24hours']);
+
+	//change : '.$arrayETH['change'].'
 	// $bot->sendMessageNew('U291587c13144989597448437db2b8555', 'Hello World !!');
 	// $bot->sendMessageNew('U291587c13144989597448437db2b8555', 'Hello World !!');
 	// $bot->sendMessageNew('U291587c13144989597448437db2b8555', 'Hello World !!');
