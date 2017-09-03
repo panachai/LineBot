7a255f517801e11faabc58ec410c53f305455878
GOLF_PANACHAI_NIYOMKODCHAKORN <panachai.ny@gmail.com>
2017-09-03 10:15:22 +0700

    dd

--- botAutoNoti.php
+++ botAutoNoti.php
@@ -19,19 +19,34 @@ if(!(date("H")<6 || date("H")>23)){ //control date
 
 	$userId = 'U291587c13144989597448437db2b8555';
 	// $lowerValue = $bar ?: $baz; // for database
-	$lowerValue = 3000;
-	$higherValue = 9500;
+	$lowerValue = 350;
+	$higherValue = 430;
+
+//ETH
+// if($userId == 'U291587c13144989597448437db2b8555'){ //ตรวจ userId จาก database (เดี๋ยวทำ)
+// 	if($arrayETH['last_price']<$lowerValue || $arrayETH['last_price']>$higherValue){
+// 		$bot->sendMessageNew($userId,
+// 		$arrayETH['primary_currency'].' : '.$arrayETH['secondary_currency'].'
+// 		LastPrice : '.$arrayETH['last_price'].'
+// 		volume_24hours : '.$arrayETH['volume_24hours']
+// 		.PHP_EOL.$arrayOMG['primary_currency'].' : '.$arrayOMG['secondary_currency'].'
+// 		LastPrice : '.$arrayOMG['last_price'].'
+// 		volume_24hours : '.$arrayOMG['volume_24hours']);
+
+		if($userId == 'U291587c13144989597448437db2b8555'){ //ตรวจ userId จาก database (เดี๋ยวทำ)
+			if($arrayOMG['last_price']<$lowerValue || $arrayOMG['last_price']>$higherValue){
+				$bot->sendMessageNew($userId,
+				$arrayETH['primary_currency'].' : '.$arrayETH['secondary_currency'].'
+				LastPrice : '.$arrayETH['last_price'].'
+				volume_24hours : '.$arrayETH['volume_24hours']
+				.PHP_EOL.$arrayOMG['primary_currency'].' : '.$arrayOMG['secondary_currency'].'
+				LastPrice : '.$arrayOMG['last_price'].'
+				volume_24hours : '.$arrayOMG['volume_24hours']);
 
 
-if($userId == 'U291587c13144989597448437db2b8555'){ //ตรวจ userId จาก database (เดี๋ยวทำ)
-	if($arrayETH['last_price']<$lowerValue || $arrayETH['last_price']>$higherValue){
-		$bot->sendMessageNew($userId,
-		$arrayETH['primary_currency'].' : '.$arrayETH['secondary_currency'].'
-		LastPrice : '.$arrayETH['last_price'].'
-		volume_24hours : '.$arrayETH['volume_24hours']
-		.PHP_EOL.$arrayOMG['primary_currency'].' : '.$arrayOMG['secondary_currency'].'
-		LastPrice : '.$arrayOMG['last_price'].'
-		volume_24hours : '.$arrayOMG['volume_24hours']);
+
+
+
 //change : '.$arrayETH['change'].'
 
 			if ($bot->isSuccess()) {
