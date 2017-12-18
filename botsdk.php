e59653e97ae6331429aeb5bfb095eadec9d71e33
Panachai <panachai.ny@gmail.com>
2017-12-18 14:48:20 +0700

    test8

--- botsdk.php
+++ botsdk.php
@@ -31,34 +31,6 @@ if (!empty($events)) {
 			
 			// // Get text sent
 			// $text = $event['message']['text'];
-			// // Get replyToken
-			// $replyToken = $event['replyToken'];
-
-			// // Build message to reply back
-			// $messages = [
-			// 	'type' => 'text',
-			// 	'text' => $text
-			// ];
-
-			// // Make a POST Request to Messaging API to reply to sender
-			// $url = 'https://api.line.me/v2/bot/message/reply';
-			// $data = [
-			// 	'replyToken' => $replyToken,
-			// 	'messages' => [$messages],
-			// ];
-			// $post = json_encode($data);
-			// $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-			// $ch = curl_init($url);
-			// curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			// curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			// curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			// curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			// $result = curl_exec($ch);
-			// curl_close($ch);
-
-			// echo $result . "\r\n";
 		}
 	}
 
@@ -70,6 +42,9 @@ if (!empty($events)) {
 	// Failed
 	echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody();
 	exit();
+
+}else{
+	$bot->replyMessageNew($bot->replyToken, "Error : events emty : " .$events);
 
 }
 
