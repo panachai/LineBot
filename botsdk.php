e2b3bf0fffc6219ae73d67a2f0a8da867073fdf6
Panachai <panachai.ny@gmail.com>
2017-12-18 13:44:02 +0700

    test4

--- botsdk.php
+++ botsdk.php
@@ -20,7 +20,7 @@ $bot = new BOT_API($channelSecret, $access_token);
 
 if (!empty($bot->isEvents)) {
 
-	$bot->replyMessageNew($bot->replyToken, json_encode($bot->message));
+	//$bot->replyMessageNew($bot->replyToken, json_encode($bot->message));
 	
 	$bot->replyMessageNew($bot->replyToken, "2 ทดสอบ : ");
 
